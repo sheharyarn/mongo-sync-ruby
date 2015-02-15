5c814ea9e15a9d917b03c4c2312483c6ca96c801
Sheharyar Naseer <sheharyar92@live.com>
2015-02-16 03:24:28 +0500

    Pull Method

--- lib/mongo_sync.rb
+++ lib/mongo_sync.rb
@@ -5,10 +5,57 @@ require 'mongo_sync/config'
 
 module MongoSync
     def self.pull(config = nil)
-    	configs = MongoSync::Configuration.parse(config)
-	end
+        configs  = MongoSync::Configuration.parse(config)
+        tmpdir   = tmp_dir
+
+        puts       "Dumping Remote DB to #{tmpdir}..." 
+        system_run "mongodump \
+                        -h #{configs['remote']['host']['url']}:#{configs['remote']['host']['port']} \
+                        -d #{configs['remote']['db']} \
+                        -u #{configs['remote']['access']['username']} \
+                        -p #{configs['remote']['access']['password']} \
+                        -o #{tmpdir} > /dev/null"
+
+        puts        "Overwriting Local DB..."
+        system_run  "mongorestore \
+                        -d #{configs['local']['db']} \
+                        #{tmpdir}/#{configs['remote']['db']} \
+                        --drop > /dev/null"
+
+        clean_up
+        done_msg
+    end
     
     def self.push(config = nil)
-    	configs = MongoSync::Configuration.parse(config)
-	end
+        configs = MongoSync::Configuration.parse(config)
+        tmpdir  = tmp_dir
+    end
+
+    
+    private
+    
+    def self.tmp_dir
+        "/tmp/mongo_sync/#{Time.now.to_i}"
+    end
+
+    def self.success_msg
+        puts 'Success!', ''
+    end
+
+    def self.done_msg
+        puts 'Done!', ''
+    end
+
+    def self.clean_up(dir)
+        puts        "Cleaning Up..."
+        system_run  "rm -rf #{dir}"
+        success_msg
+    end
+
+    def self.system_run(command)
+        raise SystemCommandFailed.new("System Command '#{command}' Failed") if not system(command)
+        success_msg
+    end
+
+    class SystemCommandFailed < StandardError; end
 end
