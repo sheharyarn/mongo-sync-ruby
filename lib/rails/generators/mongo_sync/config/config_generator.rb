0a80ba3e0f6ee622152f9253628eaa9edd5cbfbb
Sheharyar Naseer <sheharyar92@live.com>
2015-02-16 01:11:02 +0500

    Config Generator

--- lib/rails/generators/mongo_sync/config/config_generator.rb
+++ lib/rails/generators/mongo_sync/config/config_generator.rb
@@ -1,12 +1,12 @@
 module MongoSync
     module Generators
         class ConfigGenerator < Rails::Generators::Base
-            desc 'Creates a MongoSync Gem configuration file at config/mongosync.yml'
+            desc 'Creates a MongoSync Gem configuration file at config/mongo_sync.yml'
 
             source_root File.expand_path("../templates", __FILE__)
 
             def create_config_file
-                template 'mongosync.yml', File.join('config', 'mongosync.yml')
+                template 'mongo_sync.yml', File.join('config', 'mongo_sync.yml')
             end
         end
     end
