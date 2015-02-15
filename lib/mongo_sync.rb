1a8d4d1e5de78593d6f622d40f17f999f80a87b3
Sheharyar Naseer <sheharyar92@live.com>
2015-02-16 03:46:23 +0500

    MongoSync.push

--- lib/mongo_sync.rb
+++ lib/mongo_sync.rb
@@ -6,10 +6,10 @@ require 'mongo_sync/config'
 module MongoSync
     def self.pull(config = nil)
         configs  = MongoSync::Configuration.parse(config)
-        tmpdir   = tmp_dir
+        tmpdir   = new_tmp_dir
 
-        puts       "Dumping Remote DB to #{tmpdir}..." 
-        system_run "mongodump \
+        puts        "Dumping Remote DB to #{tmpdir}..." 
+        system_run  "mongodump \
                         -h #{configs['remote']['host']['url']}:#{configs['remote']['host']['port']} \
                         -d #{configs['remote']['db']} \
                         -u #{configs['remote']['access']['username']} \
@@ -22,19 +22,36 @@ module MongoSync
                         #{tmpdir}/#{configs['remote']['db']} \
                         --drop > /dev/null"
 
-        clean_up
+        clean_up(tmpdir)
         done_msg
     end
     
     def self.push(config = nil)
         configs = MongoSync::Configuration.parse(config)
-        tmpdir  = tmp_dir
+        tmpdir  = new_tmp_dir
+
+        puts        "Dumping Local DB to #{tmpdir}..."
+        system_run  "mongodump \
+                        -d #{configs['local']['db']} \
+                        -o #{tmpdir} > /dev/null"
+
+        puts        "Overwriting Remote DB with Dump..."
+        system_run  "mongorestore \
+                        -h #{configs['remote']['host']['url']}:#{configs['remote']['host']['port']} \
+                        -d #{configs['remote']['db']} \
+                        -u #{configs['remote']['access']['username']} \
+                        -p #{configs['remote']['access']['password']} \
+                        #{tmpdir}/#{configs['remote']['db']} \
+                        --drop > /dev/null"
+
+        clean_up(tmpdir)
+        done_msg
     end
 
     
     private
     
-    def self.tmp_dir
+    def self.new_tmp_dir
         "/tmp/mongo_sync/#{Time.now.to_i}"
     end
 
